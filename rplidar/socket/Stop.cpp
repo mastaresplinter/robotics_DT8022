a56fc8ec3bf121f5579a512a2de30727438ee761
mastaresplinter <72516224+mastaresplinter@users.noreply.github.com>
2023-04-06 11:23:15 +0200

    added objectdet and sockets

---
+++ rplidar/socket/Stop.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <arpa/inet.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 9887
+using namespace std;
+
+int main()
+{
+    int status,sockkfd;
+    struct sockaddr_in serv_addr;
+    char startmsg[2];
+    startmsg[1] = char(0);
+    startmsg[0] = char(32);
+    
+    sockkfd = socket(AF_INET,SOCK_STREAM,0);
+    
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    
+    if ((status
+         = connect(sockkfd, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    printf("\n Connection Succesful");
+    printf("\n0x%x", startmsg[0]);
+    printf("\n0x%x", startmsg[1]);
+    send(sockkfd, &startmsg, 2, 0);
+    return 0;
+}
